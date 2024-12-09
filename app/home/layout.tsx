1f53682dfdceb8a0486ea726c4987e397fdca882
tr-ggr <tristanjamestolentino56@gmail.com>
2024-12-09 22:02:47 +0800

    LAST WORKING LMAO

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -5,6 +5,7 @@ import localFont from "next/font/local";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell, faGear } from "@fortawesome/free-solid-svg-icons";
 import { SideBar } from "./chats"
+
 export default function HomeLayout({
   children,
 }: Readonly<{
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://localhost:7113/api/:path*',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
